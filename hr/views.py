7568c34a86090aea8323d8b8a64521569e1dbf0b
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-11-06 19:49:34 +0000

    aa

--- hr/views.py
+++ hr/views.py
@@ -6,7 +6,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 
-class HRTemplateView(LoginRequiredMixin, TemplateView):
+class HRTemplateView(TemplateView):
     """Vue de base pour les templates HR"""
 
     @method_decorator(ensure_csrf_cookie)
