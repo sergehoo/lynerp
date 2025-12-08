3c44879b0998ae8bc73eb53f9a021621522f358f
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-12-08 19:14:06 +0000

    aa

--- hr/api/views.py
+++ hr/api/views.py
@@ -588,6 +588,7 @@ class EmployeeViewSet(BaseTenantViewSet, viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     permission_classes = [IsAuthenticated]
+    # authentication_classes = [JWTAuthentication]
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['first_name', 'last_name', 'email', 'matricule']
     ordering_fields = ['first_name', 'last_name', 'hire_date', 'created_at']
