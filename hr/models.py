d246a28188d9c2be36c06edaeeb30476a52e9f16
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-12-08 20:29:17 +0000

    aa

--- hr/models.py
+++ hr/models.py
@@ -1,5 +1,7 @@
 # Lyneerp/hr/models.py
 import uuid
+
+from django.conf import settings
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils import timezone
